b74cbf72b2597e2b0df7f8e8058b1c933a74be3c
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-17 10:38:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NullReferenceLogic
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+             try
+                {
+                Console.WriteLine("enter string val");
+                string val = null;  
+                Console.WriteLine(val.Length);
+                
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("null reference logic"+ex.Message);
+                }
+            }
+        }
+    
+}
